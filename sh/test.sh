99639cefa6b9fc4ac2a15511b213be538d7301b5
syd <sydnever@users.noreply.github.com>
2017-08-11 13:31:12 +0800

    a mistake

--- sh/test.sh
+++ sh/test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./ociuldr user="tytest/tytest" query="select ENAME,JOB,SAL,COMM,dbms_lob.substr(clobs),NULL,to_char(sysdate,'YYYY-MM-DD HH24:MI:SS'),'INSERT','tytest',1,NULL,NULL,NULL,NULL,1,rowid from tytest.TEST_UNIQUE_KEY t" field=",_," escape='^A'  quote='^' null='^AN'   file="/home/oracle/ociuldr/TEST_UNIQUE_KEY.txt" 
+./ociuldr user="tytest/tytest@orcl" query="select ENAME,JOB,SAL,COMM,dbms_lob.substr(clobs),NULL,to_char(sysdate,'YYYY-MM-DD HH24:MI:SS'),'INSERT','tytest',1,NULL,NULL,NULL,NULL,1,rowid from tytest.TEST_UNIQUE_KEY t" field=",_," escape='^A'  quote='^' null='^AN'   file="/home/oracle/ociuldr/TEST_UNIQUE_KEY.txt" 
